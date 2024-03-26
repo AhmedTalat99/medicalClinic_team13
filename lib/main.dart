ba55c3de689e1a364a5c7f92e746af5a45cf852f
maher <maherrmaherr.gmail.com>
2024-03-26 22:07:18 +0200

    Homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:medical_clinic_team13/featurs/homescreen/ui/Homepage.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    home: homescreen(),
-  ));
+  runApp(const Text(''));
 }
